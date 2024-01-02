4a08b28f4b3dbdc7a3b9b1280347e088821d1d9c
Azumi <outcast.pathetic@vk.com>
2024-01-02 20:43:47 +0500

    landing created

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HeaderComp from './components/HeaderComp.vue';
+import FooterComp from './components/FooterComp.vue';
 </script>
 
 <template>
   <HeaderComp/>
   <RouterView />
+  <FooterComp />
 </template>
 
 <style>
---
+++ src/components/FooterComp.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+</script>
+<script>
+
+</script>
+
+<template>
+    <footer>
+        <h1>Популярные маршруты совместных поездок</h1>
+        <div class="routes_list">
+            <h3>Москва → Санкт-Петербург</h3>
+            <h3>Челябинск → Екатеринбург</h3>
+            <h3>Санкт-Петербург → Москва</h3>
+            <h3>Тольятти → Самара</h3>
+            <h3>Екатеринбург → Челябинск</h3>
+            <h3>Барнаул → Новосибирск</h3>
+        </div>
+    </footer>
+
+</template>
+
+<style scoped lang="scss">
+footer{
+    background-color: #cfcece;
+    display: flex;
+    flex-direction: column;
+    padding: 0 50px;
+    h1{
+        color: #23272e;
+        font-family: "Exo 2";  
+    }
+    h3{
+        color: #23272e;
+        font-family: "Exo 2"; 
+    }
+    .routes_list{
+        display: flex;
+        justify-content: space-between;
+    }
+}
+</style>
